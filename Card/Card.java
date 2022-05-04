4fbd596af7bd59e52881d141aa53223a21588dcd
NguyenPhuong <102490522+NguyenPhuong242@users.noreply.github.com>
2022-05-04 16:44:58 +0200

    lan 1

---
+++ Card/Card.java
@@ -0,0 +1,57 @@
+public class Card {
+    private String name;
+    private boolean shiny;
+    private double value;
+    private int year;
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean isShiny(){
+        return shiny;
+    }
+
+    public double getValue(){
+        return value;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public Card(String name,boolean shiny, double value, int year){
+        this.name = name;
+        this.shiny = shiny;
+        this.value = value;
+        this.year = year;
+    }
+
+    public Card(Card card){
+        this.name = card.name;
+        this.shiny = card.shiny;
+        this.value = card.value;
+        this.year = card.year;
+    }
+
+    public boolean equals(Card card){
+        if (card == null){
+            return false;
+        }
+        
+        if (card instanceof Card){
+            if (this.name == card.name 
+            && this.shiny == card.shiny
+            && this.value == card.value
+            && this.year == card.year){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public String toString(){
+        return name + " " + shiny + " " + value + " " + year;
+    }
+}
--- Card/Collector.java
+++ Card/Collector.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collector {
-    
+    private Card card;
+    private List<Card> cards = new ArrayList<>();
+
+    public Card getCard(){
+        return card;
+    }
+
+    public List<Card> getList(){
+        return cards;
+    }
+
+    public List<Card> addCard(Card card){
+        List<Card> cardclone = new ArrayList<>();
+        for(int i = 0; i < cards.size() + 1; i++){
+            cardclone.add(card);
+        }
+        cards = cardclone;
+        return cards;
+
+    }
+
+    public List<Card> getCards(Card card){
+        List<Card> cardget = new ArrayList<>();
+        for(int j = 0; j < cards.size(); j++){
+            ((Collector) cardget).getCards(card);
+        }
+        return cardget;
+    }
 }
